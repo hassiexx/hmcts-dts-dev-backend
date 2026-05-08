fc07fc0e032bf3871c45595e826ff2c3c8ecba94
Hassie <hi@hassie.boo>
2026-05-08 20:55:01 +0100

    Init project

---
+++ src/test/java/boo/hassie/java/hmcts/dts/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package boo.hassie.java.hmcts.dts.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
